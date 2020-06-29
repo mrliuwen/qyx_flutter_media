884aa76087f2c3a1709625e180d6885f516d90bc
liuwen <745685567@qq.com>
2020-06-29 11:34:33 +0800

    :update:

--- lib/qyx_flutter_media.dart
+++ lib/qyx_flutter_media.dart
@@ -25,9 +25,18 @@ class QyxFlutterMedia {
     String videoPath = resultMap["video_path"];
     String photoPath = resultMap["photo_path"];
     int time = resultMap["time"]??0;
-    File coverFile= File(coverPath);
-    File videoFile= File(videoPath);
-    File photoFile= File(photoPath);
+    File coverFile;
+    File videoFile;
+    File photoFile;
+    if(coverPath!=null&&coverPath.length>0){
+      coverFile= File(coverPath);
+    }
+    if(videoPath!=null&&videoPath.length>0){
+      videoFile= File(videoPath);
+    }
+    if(photoPath!=null&&photoPath.length>0){
+      photoFile= File(photoPath);
+    }
     return MediaInfo(coverFile: coverFile,videoFile: videoFile,time: time,photoFile: photoFile);
   }
 
