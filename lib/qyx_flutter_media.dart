776d8998de68c2b3f2ec01a21f27a19381316901
liuwen <745685567@qq.com>
2020-06-29 12:01:31 +0800

    :update:

--- lib/qyx_flutter_media.dart
+++ lib/qyx_flutter_media.dart
@@ -20,24 +20,17 @@ class QyxFlutterMedia {
   /// 拍照
   static Future<MediaInfo> takeVideo() async {
     Map resultMap = await _channel.invokeMethod("takeVideo");
-    print('resultMap---$resultMap');
-    String coverPath = resultMap["cover_path"];
     String videoPath = resultMap["video_path"];
     String photoPath = resultMap["photo_path"];
-    int time = resultMap["time"]??0;
-    File coverFile;
     File videoFile;
     File photoFile;
-    if(coverPath!=null&&coverPath.length>0){
-      coverFile= File(coverPath);
-    }
     if(videoPath!=null&&videoPath.length>0){
       videoFile= File(videoPath);
     }
     if(photoPath!=null&&photoPath.length>0){
       photoFile= File(photoPath);
     }
-    return MediaInfo(coverFile: coverFile,videoFile: videoFile,time: time,photoFile: photoFile);
+    return MediaInfo(videoFile: videoFile,photoFile: photoFile);
   }
 
 }
